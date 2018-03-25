7e7977b16a3c782a19c06d7f081fcfacc283d7ab
hwinn4 <7FiEKMDuRrW2(Ykk>
2018-03-25 18:24:27 -0400

    updates for 2018

--- app.rb
+++ app.rb
@@ -3,23 +3,25 @@ require 'sinatra'
 
 class Students
   STUDENTS = [
-      { first: 'Ahmed', last: 'Abdalla' },
-      { first: 'David' , last: 'Amato' },
-      { first: 'Evelyn', last: 'Ariza' },
-      { first: 'Meagan', last: 'Carioti'}
+      { first: 'Ahmed', last: 'Abdalla', grad_year: 2017 },
+      { first: 'David' , last: 'Amato', grad_year: 2017 },
+      { first: 'Evelyn', last: 'Ariza', grad_year: 2017 },
+      { first: 'Dakarai', last: 'Arnold', grad_year: 2018 },
+      { first: 'Meagan', last: 'Carioti', grad_year: 2017 },
+      { first: 'Abdel', last: 'Khedr', grad_year: 2018 },
+      { first: 'Christy', last: 'Li', grad_year: 2018 },
+      { first: 'Kathleen', last: 'Saloma', grad_year: 2018 },
     ]
 end
 
 class Student
-  attr_reader :first, :last
+  attr_reader :first, :last, :grad_year
 
   def initialize(student_hash)
+    puts student_hash
     @first = student_hash[:first]
     @last = student_hash[:last]
-  end
-
-  def grad_year
-    2017
+    @grad_year = student_hash[:grad_year]
   end
 end
 
